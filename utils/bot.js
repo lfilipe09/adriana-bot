727fee289abdb9d5101ddf4165d84f5178cf95e8
lfilipe09 <luis.andrade987@gmail.com>
2023-06-22 01:19:01 -0300

    fix: finish onboarding features

--- utils/bot.js
+++ utils/bot.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const config = {
-  headers: { 'Authorization': 'Bearer EAANvl8azHFIBAL1vnJnlwInmIjL38oTs2K9iGELlZBxwXqSo0ZAuteuSF55Waiql6GpaB3ljgEF53Xc3NKkPiUzisOHP8CYmdhaqxHQIfjw3ZAfkYOglLTQQYEH6VKYihzoj43TqQW8YO8oIgLeiTabWEJWrzZCsjDaD1APh3UdBUAli1xnx', 'Content-Type': 'application/json'}
+  headers: { 'Authorization': proccess.env.FACETOKEN, 'Content-Type': 'application/json'}
 };
 
 async function answerUser (template) {
